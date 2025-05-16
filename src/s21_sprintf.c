355ec46d019195a5382a3cbeedf2921b40472b91
earthwalkerqq <ilya.assanov.03@mail.ru>
2025-05-17 01:09:27 +0300

    continue make s12_sprintf function

--- src/s21_sprintf.c
+++ src/s21_sprintf.c
@@ -2,10 +2,10 @@
 #include <stdarg.h>
 #include "s21_string.h"
 
-
-char *s21_sprintf(char *buffer, char const *str, ...) {
+int *s21_sprintf(char *buffer, char const *str, ...) {
     int num1 = 0, num2 = 0;
     int i = 0;
+    char *tmp = NULL;
     va_list list;
     va_start(list, str);
     while (*str) {
@@ -19,20 +19,124 @@ char *s21_sprintf(char *buffer, char const *str, ...) {
                     num2 = modificationNumber(&str);
                 }
             }
-            
+            switch (*str++)
+            {
+            case 'X':
+            case 'x':
+                tmp = convertToNumSys(va_arg(list, unsigned int), 16);
+                size_t len = s21_strlen(tmp);
+                if ((num1 - len) > 0) {
+                    for (int j = 0; j <= num1; j++, i++) {
+                        buffer[i] = ' ';
+                    }
+                }
+                buffer[i++] = '\0';
+                s21_strncat(buffer, tmp, len);
+                break;
+            case 'D':
+            case 'd':
+                tmp = itoa(va_arg(list, int));
+                size_t len = s21_strlen(tmp);
+                if ((num1 - len) > 0) {
+                    for (int j = 0; j <= num1; j++, i++) {
+                        buffer[i] = ' ';
+                    }
+                }
+                buffer[i++] = '\0';
+                s21_strncat(buffer, tmp, len);
+                break;
+            case 'C':
+            case 'c':
+                size_t len = 1;
+                if ((num1 - len) > 0) {
+                    for (int j = 0; j <= num1; j++, i++) {
+                        buffer[i] = ' ';
+                    }
+                }
+                buffer[i++] = '\0';
+                s21_strncat(buffer, va_arg(list, char), len);
+                break;
+            case 'I':
+            case 'i':
+                // доделать спецификаторы
+                break;
+            case 'O':
+            case 'o':
+                tmp = convertToNumSys(va_arg(list, unsigned int), 8);
+                size_t len = s21_strlen(tmp);
+                if ((num1 - len) > 0) {
+                    for (int j = 0; j <= num1; j++, i++) {
+                        buffer[i] = ' ';
+                    }
+                }
+                buffer[i++] = '\0';
+                s21_strncat(buffer, tmp, len);
+                break;
+            case 'S':
+            case 's':
+                size_t len = 1;
+                if ((num1 - len) > 0) {
+                    for (int j = 0; j <= num1; j++, i++) {
+                        buffer[i] = ' ';
+                    }
+                }
+                buffer[i++] = '\0';
+                s21_strncat(buffer, va_arg(list, char*), len);
+                break;
+            case 'f':
+
+                break;
+            }
         } else {
             buffer[i++] = *(str - 1);
         }
     }
+    buffer[i] = '\0';
     va_end(list);
+    return i + 1;
 }
 
-void convertToNumSys(char **buffer, size_t size, unsigned int num, int numSys) {
-    char *p = *buffer;
-    p[size - 1] = '\0';
-    while (num) {
-        *--p = "0123456789abcdef"[num % numSys];
-        num /= numSys;
+char *itoa(int num) {
+    char *res = convertToNumSys((unsigned int)num, 10);
+    if (num < 0) {
+        int len = s21_strlen(res);
+        len;
+        res = realloc(res, (len + 1) * sizeof(char));
+        for (int i = len; i > 0; i--) res[i] = res[i - 1];
+        *res = '-';
+    }
+    return res;
+}
+
+char *itof(float num) {
+    int len = 2;
+    char *res = malloc(sizeof(char));
+    for (; )
+}
+
+// возбмет десятичнуб чвасть по параметру округления 
+int getDecimalPart(float val, int deg) {
+    int num = (int)val;
+    // дописать
+}
+
+char *convertToNumSys(unsigned int num, int numSys) {
+    int len = 2;
+    char *res = malloc(sizeof(char));
+    for (int i = 0; num != 0; i++, len++, num /= numSys) {
+        res = realloc(res, len * sizeof(char));
+        res[i] = "0123456789abcdef"[num % numSys];
+    }
+    res[len - 1] = '\0';
+    conversely(res, len);
+    return res;
+}
+
+void conversely(char *str, int len) {
+    for (int st = 0, fn = len - 2; st < fn; st++, fn--) {
+        char temp = str[st];
+        str[st] = str[fn];
+        str[fn] = temp;
     }
 }
 
